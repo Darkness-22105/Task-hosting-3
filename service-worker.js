6bbe6fc10a4517b79e80985133e500e146b66032
Aryan Pawar <aryanzeal22105@gmail.com>
2025-07-07 12:07:29 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+// service-worker.js
+
+const CACHE_NAME = 'aryan-portfolio-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
